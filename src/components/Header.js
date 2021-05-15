4668916ae368b58cf87e072654b7b3d0c128e302
faizaslam11 <faizaslam11@gmail.com>
2021-05-15 10:15:09 +0530

    Header for tesla

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,32 @@
 import React from 'react'
-
+import styled from "styled-components"
 function Header() {
     return (
-        <div>
-        header
-            
-        </div>
+        <Container>
+        <a>
+            <img src="images/logo.svg"></img>
+        </a>  
+        <Menu>
+        <p><a href="#">Model S</a> </p>
+        <p><a href="#">Model 3</a> </p>
+        <p><a href="#">Model X</a> </p>
+        <p><a href="#">Model Y</a> </p>
+
+
+        </Menu>          
+        </Container>
     )
 }
 
 export default Header
 
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+`
+const Menu = styled.div`
+
+`
