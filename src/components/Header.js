5e6e534ec2ea83276d9ee3a356fdde46f465e0ed
faizaslam11 <faizaslam11@gmail.com>
2021-05-15 08:17:48 +0530

    aa

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+        header
+            
+        </div>
+    )
+}
+
+export default Header
+
---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+function Section() {
+    return (
+        <Wrap>
+            Section
+        </Wrap>
+    )
+}
+
+export default Section
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: orange;
+    `
